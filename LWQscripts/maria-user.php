d530ceb3cccb75770c9241fb9b28e320cc9962cb
M0n0chr0mic0rn <m0n0chr0mic0rn@proton.me>
2023-11-10 16:33:39 +0100

    modified:   LWQscripts/maria-user.php     -> Email Provider List

--- LWQscripts/maria-user.php
+++ LWQscripts/maria-user.php
@@ -2,48 +2,60 @@
 require_once("../LWQscripts/key.php");
 
 class User {
-    private static $_db_username = 'maria';
-    private static $_db_passwort = 'KerkerRocks22';
-    private static $_db_host = '127.0.0.1';
-    private static $_db_name = 'API_user';
+    private static $_db_username = "maria";
+    private static $_db_passwort = "KerkerRocks22";
+    private static $_db_host = "127.0.0.1";
+    private static $_db_name = "API_user";
     private static $_db;
     
     function __construct(){
         try{
-            self::$_db = new PDO('mysql:host=' . self::$_db_host . ';dbname=' . self::$_db_name, self::$_db_username, self::$_db_passwort);
+            self::$_db = new PDO("mysql:host=" . self::$_db_host . ";dbname=" . self::$_db_name, self::$_db_username, self::$_db_passwort);
         }catch(PDOException $e){
-            echo '<br>DATABASE ERROR<br>'.$e;
+            echo "<br>DATABASE ERROR<br>".$e;
             die();
         }
     }
 
     function hello($RETURN) {
         // The "Hello World" Example
-        $RETURN->answer = 'Hello World, I am a Litecoin-Node-API. How can I help U?';
+        $RETURN->answer = "Hello World, I am a Litecoin-Node-API. How can I help U?";
         $RETURN->bool = true;
-        $RETURN->ip = $_SERVER['REMOTE_ADDR'];
+        $RETURN->ip = $_SERVER["REMOTE_ADDR"];
 
         // Get total amount of users
-        $stmt = self::$_db->prepare('SELECT * FROM user');
+        $stmt = self::$_db->prepare("SELECT * FROM user");
         $stmt->execute();
         $RETURN->total_users = $stmt->rowCount();
 
         // The command lists
         $RETURN->commands = array(
-            'public'=>'list public commands',
-            'user'=>'list user commands',
-            'omni'=>'list omni commands'
+            "public"=>"list public commands",
+            "user"=>"list user commands",
+            "omni"=>"list omni commands"
         );
 
         // List the working email providers
-        // TO DO: IMPROVE BY READOUT THE PROVIDERS FROM USER TABLE
-        $stmt = self::$_db->prepare('SELECT * FROM mailprovider');
+        // Select all Emails from the user table and store it in data
+        $stmt = self::$_db->prepare("SELECT Mail FROM user");
         $stmt->execute();
         $data = $stmt->fetchAll(PDO::FETCH_ASSOC);
 
-        for ($a=0; $a < $stmt->rowCount(); $a++) { 
-            $array[$a] = $data[$a]['Provider'];
+        // Create new array
+        $array = array();
+
+        // Loop throw data and add every provider in a unique list
+        for ($a=0; $a < count($data); $a++) {
+            // Cut of the user from the Email
+            $provider = explode("@", $data[$a]["Mail"])[1];
+
+            // If provider is not present in the list add it
+            if (array_search($provider, $array) === false) {
+                $array[count($array)] = $provider;
+            }
         }
+
+        // attach the provider list to the RETURN object
         $RETURN->mailprovider = $array;
 
         return $RETURN;
