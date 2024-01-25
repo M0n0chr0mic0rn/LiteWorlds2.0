4f14d49cf5cb5e5cc1a45c1ab7703acee022d9d6
M0n0chr0mic0rn <m0n0chr0mic0rn@proton.me>
2024-01-25 04:31:18 +0100

    Just some style/info update 	modified:   LWQscripts/user.php

--- LWQscripts/user.php
+++ LWQscripts/user.php
@@ -11,17 +11,17 @@ if ($_GET["method"] == "user")
     $RETURN->answer = "Here are the user commands.";
     $RETURN->bool = true;
     $RETURN->commands = array(
-        "user-execute"=>"Sign a prepared action",
-        "user-iplock"=>"Turn on or off the IP lock, always IP locked",
-        "user-register"=>"Prepares a register action, always IP locked",
-        "user-login"=>"Prepares a login action",
-        "user-logout"=>"Logout the user and delete its authkey from database, no sign needed, always IP locked",
-        "user-get"=>"Get public user data",
-        "user-change-mail"=>"Prepares a mail change action, need to be signed from both Emails",
-        "user-change-pass"=>"Prepares a pass change action",
-        "user-pass-recovery"=>"Set a new password if it get lost, auth by username and email, always IP locked",
-        "user-pairing"=>"Start the self-custody pairing process",
-        "user-pairing-sign"=>"Complete the self-custody pairing process"
+        "user-execute"=>"Sign a prepared action, 2FA Function gets called by system itself",
+        "user-iplock"=>"Turn on or off the IP lock, always IP locked, Parameters: authkey",
+        "user-register"=>"Prepares a register action, always IP locked, Parameters: user, pass, mail",
+        "user-login"=>"Prepares a login action, Parameters: user, pass",
+        "user-logout"=>"Logout the user and delete its authkey from database, no sign needed, always IP locked, Parameters: authkey",
+        "user-get"=>"Get public user data, Parameters: authkey",
+        "user-change-mail"=>"Prepares a mail change action, need to be signed from both Emails, Parameters: authkey, mail",
+        "user-change-pass"=>"Prepares a pass change action, Parameters: authkey, pass",
+        "user-pass-recovery"=>"Set a new password if it get lost, auth by username and email, always IP locked, Parameters: user, mail, pass",
+        "user-pairing"=>"Start the self-custody pairing process, Parameters: authkey, address",
+        "user-pairing-sign"=>"Complete the self-custody pairing process, Parameters: authkey, txid"
     );
 
     // print the RETURN as JSON
